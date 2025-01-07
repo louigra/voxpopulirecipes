83c6ffe87cf3a52b860a29ae3ffdc91abeb4b27d
Grant Louis <glouis6@me.com>
2025-01-07 14:28:57 -0500

    s3 modification

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -150,11 +150,10 @@ USE_TZ = True
 USE_S3 = False
 
 if USE_S3:
-    # AWS S3 Static and Media settings
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = 'voxpopulirecipes'
-    AWS_S3_REGION_NAME = 'us-east-2'
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-2')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_DEFAULT_ACL = None
     AWS_QUERYSTRING_AUTH = False
