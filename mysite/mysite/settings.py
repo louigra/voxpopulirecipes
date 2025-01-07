2d460abda6146d25d3e1df830cf11fe0e22c00a4
Grant Louis <glouis6@me.com>
2025-01-07 13:56:49 -0500

    s3 modification

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-USE_S3 = os.environ.get('USE_S3') == 'TRUE'
+USE_S3 = True
 
 if USE_S3:
     # aws settings
