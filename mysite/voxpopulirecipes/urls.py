e847a30b604b85328eb5f1ac400909a9e8f40ca7
Grant Louis <glouis6@me.com>
2025-02-04 12:23:40 -0500

    reseting

--- mysite/voxpopulirecipes/urls.py
+++ mysite/voxpopulirecipes/urls.py
@@ -22,8 +22,7 @@ urlpatterns = [
     path("star_recipe/<int:recipe_id>/", views.star_recipe, name="star_recipe"),
     path("view-user-book/<int:user_id>/", views.view_user_book, name="view_user_book"),
     path("submit_recipe_selector/", views.submit_recipe_selector, name="submit_recipe_selector"),
-    path('extract-text/', views.extract_recipe_text, name='extract_recipe_text'),  # Image processing
-    path('parse-recipe/', views.parse_recipe, name='parse_recipe'),  # Process text
-    path('upload-image/', views.parse_image_recipe, name='parse_image_recipe'),  # Image template
-    path('submit-text/', views.parse_text_recipe, name='parse_text_recipe'),  # Text template
+    path("parse_recipe/", views.parse_recipe, name="parse_recipe"),
+    path("submit_recipe_from_text/", views.submit_recipe_from_text, name="submit_recipe_from_text"),
+    path("submit_recipe_from_image/", views.submit_recipe_from_image, name="submit_recipe_from_image"),
 ]
